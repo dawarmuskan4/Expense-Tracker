5fe6d1cca5242eeface172a089378f99648b8556
Muskan Dawar <sanchitdawar34@gmail.com>
2022-01-13 12:33:48 +0530

    first

---
+++ src/Context/AppReducer.js
@@ -0,0 +1,19 @@
+const AppReducer = (state,action) => {
+
+  switch(action.type){
+    case 'Delete_transaction':
+      return{
+        ...state,
+        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+      }
+    case 'Add_transaction':
+      return{
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      }
+    default:
+      return state;
+  }
+}
+
+export default AppReducer;
